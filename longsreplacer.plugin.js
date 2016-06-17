4604ecf8b595b68e1a184580cd2f88d14af67660
Iratu <admin@rori-dl.com>
2016-06-17 14:06:37 +0200

    Initial Commit

---
+++ longsreplacer.plugin.js
@@ -0,0 +1,55 @@
+//META{"name":"sreplacer"}*//
+
+
+
+
+
+var sreplacer = function () {};
+
+sreplacer.prototype.convert = function () {
+    $(".markup").each(function() { 
+		var msg = $(this).text();
+		if(msg.indexOf("ſ") >= 0){
+			msg = msg.replaceAll("ſ","s",true);
+			$(this).text(msg);
+		}
+    });
+};
+
+sreplacer.prototype.onMessage = function () {
+    this.convert();
+};
+sreplacer.prototype.onSwitch = function () {
+    this.convert();
+};
+sreplacer.prototype.start = function () {
+    this.convert();
+};
+
+sreplacer.prototype.load = function () {};
+
+sreplacer.prototype.unload = function () {};
+
+sreplacer.prototype.stop = function () {};
+
+sreplacer.prototype.getSettingsPanel = function () {
+    return "";
+};
+sreplacer.prototype.getName = function () {
+    return "ſ Replacer";
+};
+sreplacer.prototype.getDescription = function () {
+    return "Replaces the long s (ſ) and turns it into a regular s.";
+};
+sreplacer.prototype.getVersion = function () {
+    return "0.0.1";
+};
+sreplacer.prototype.getAuthor = function () {
+    return "Iratu";
+};
+
+
+String.prototype.replaceAll = function(str1, str2, ignore) 
+{
+    return this.replace(new RegExp(str1.replace(/([\/\,\!\\\^\$\{\}\[\]\(\)\.\*\+\?\|\<\>\-\&])/g,"\\$&"),(ignore?"gi":"g")),(typeof(str2)=="string")?str2.replace(/\$/g,"$$$$"):str2);
+} 
